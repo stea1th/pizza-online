7e1fd8bcebd4d694514e21a260e1e7c6892f6592
vlasov <stea1th@mail.ru>
2020-01-15 13:54:59 +0100

    kafka-library module

---
+++ kafka-library/src/main/java/de/stea1th/kafkalibrary/KafkaLibraryApplication.java
@@ -0,0 +1,13 @@
+package de.stea1th.kafkalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaLibraryApplication.class, args);
+    }
+
+}
---
+++ kafka-library/src/test/java/de/stea1th/kafkalibrary/KafkaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stea1th.kafkalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
