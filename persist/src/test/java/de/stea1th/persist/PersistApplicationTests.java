2f90c3c4ae02cf5f15483c66b97534b9be5e47e5
vlasov <stea1th@mail.ru>
2020-01-15 12:18:52 +0100

    init persist

---
+++ persist/src/test/java/de/stea1th/persist/PersistApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stea1th.persist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
