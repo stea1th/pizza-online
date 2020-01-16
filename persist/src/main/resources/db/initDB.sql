8e7e8540084e32f8b2fa3c55bd1377ff7f2f9f6f
vlasov <stea1th@mail.ru>
2020-01-16 12:08:07 +0100

    initDB tables created

---
+++ persist/src/main/resources/db/initDB.sql
@@ -0,0 +1,72 @@
+
+DROP TABLE IF EXISTS CONDITION_PRODUCT;
+DROP TABLE IF EXISTS ORDER_PRODUCT;
+DROP TABLE IF EXISTS CONDITION;
+DROP TABLE IF EXISTS ORDERS;
+DROP TABLE IF EXISTS PRODUCT;
+DROP TABLE IF EXISTS PERSON;
+
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START 1000;
+
+
+CREATE TABLE PERSON
+(
+    id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    first_name VARCHAR(40) NOT NULL,
+    last_name  VARCHAR(40) NOT NULL,
+    email      VARCHAR(50) NOT NULL
+);
+
+CREATE UNIQUE INDEX person_unique_email_index ON PERSON (email);
+
+CREATE TABLE PRODUCT
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    description VARCHAR(100) NOT NULL,
+    picture     VARCHAR(100)
+);
+
+CREATE TABLE ORDERS
+(
+    id        INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    person_id INTEGER NOT NULL,
+    created   TIMESTAMP,
+    completed BOOLEAN NOT NULL    DEFAULT FALSE,
+    FOREIGN KEY (person_id) REFERENCES PERSON (id) ON DELETE CASCADE
+);
+
+CREATE TABLE CONDITION
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    description VARCHAR(100) NOT NULL,
+    price    NUMERIC(5, 2)       DEFAULT 0,
+    discount INTEGER             DEFAULT 0
+);
+
+-- CREATE TABLE COST
+-- (
+--     id       INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+--     price    NUMERIC(5, 2)       DEFAULT 0,
+--     discount INTEGER             DEFAULT 0
+-- );
+
+CREATE TABLE ORDER_PRODUCT
+(
+    order_id   INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity   INTEGER NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES PRODUCT (id) ON DELETE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES ORDERS (id) ON DELETE CASCADE,
+    PRIMARY KEY (order_id, product_id)
+);
+
+CREATE TABLE CONDITION_PRODUCT
+(
+    condition_id INTEGER NOT NULL,
+    product_id   INTEGER NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES PRODUCT (id) ON DELETE CASCADE,
+    FOREIGN KEY (condition_id) REFERENCES CONDITION (id) ON DELETE CASCADE,
+    PRIMARY KEY (condition_id, product_id)
+)
