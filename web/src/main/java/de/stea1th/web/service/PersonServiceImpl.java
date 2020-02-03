01044a26dd806886fae46879701a1e7e4a1b807e
vlasov <stea1th@mail.ru>
2020-02-03 14:00:28 +0100

    fix

--- web/src/main/java/de/stea1th/web/service/PersonServiceImpl.java
+++ web/src/main/java/de/stea1th/web/service/PersonServiceImpl.java
@@ -6,6 +6,7 @@ import de.stea1th.web.kafka.PersonKafkaConsumer;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -21,6 +22,9 @@ public class PersonServiceImpl implements PersonService {
     private final static Integer ATTEMPT = 5;
     private final static Integer SLEEP_DURATION = 100;
 
+    @Value("${person.get.id}")
+    private String personGetIdTopic;
+
     @Autowired
     public PersonServiceImpl(KafkaProducer kafkaProducer, PersonKafkaConsumer personKafkaConsumer) {
         this.kafkaProducer = kafkaProducer;
@@ -30,7 +34,7 @@ public class PersonServiceImpl implements PersonService {
     @SneakyThrows
     @Override
     public PersonDto get(int id) {
-        kafkaProducer.produce("pizza-online.kafka.get.id", "pizza-online", id);
+        kafkaProducer.produce(personGetIdTopic, "pizza-online", id);
         PersonDto personDto = null;
         for (int i = 0; i < ATTEMPT; i++) {
             TimeUnit.MILLISECONDS.sleep(SLEEP_DURATION);
