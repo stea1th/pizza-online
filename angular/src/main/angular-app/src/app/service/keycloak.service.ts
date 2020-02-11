e34dde44a7ac347ec2fb1b59a630dbab1db4360c
vlasov <stea1th@mail.ru>
2020-02-11 11:20:37 +0100

    keycloak service and json

--- angular/src/main/angular-app/src/app/service/keycloak.service.ts
+++ angular/src/main/angular-app/src/app/service/keycloak.service.ts
@@ -7,21 +7,47 @@ declare var Keycloak: any;
 })
 export class KeycloakService {
 
-  private keycloakAuth: any;
+  // private keycloakAuth: any;
+  //
+  // constructor() {
+  // }
+  //
+  // init(): Promise<any> {
+  //   return new Promise((resolve, reject) => {
+  //     const config = {
+  //       'url': 'http://localhost:4200',
+  //       'realm': 'Pizza-Online Realm',
+  //       'clientId': 'pizza-online-frontend'
+  //     };
+  //     this.keycloakAuth = new Keycloak(config);
+  //     this.keycloakAuth.init({onLoad: 'login-required'})
+  //       .success(() => {
+  //         resolve();
+  //       })
+  //       .error(() => {
+  //         reject();
+  //       });
+  //   });
+  // }
+  //
+  // getToken(): string {
+  //   return this.keycloakAuth.token;
+  // }
 
-  constructor() {
-  }
+  // private keycloakAuth: any;
+  static auth: any = {};
 
   init(): Promise<any> {
     return new Promise((resolve, reject) => {
-      const config = {
-        'url': 'http://localhost:4200',
-        'realm': 'Pizza-Online Realm',
-        'clientId': 'pizza-online-frontend'
-      };
-      this.keycloakAuth = new Keycloak(config);
-      this.keycloakAuth.init({onLoad: 'login-required'})
-        .success(() => {
+      const keycloakAuth = Keycloak('assets/keycloak/keycloak.json');
+      keycloakAuth.init({onLoad: 'login-required'})
+        .done(() => {
+          KeycloakService.auth.loggedIn = true;
+          KeycloakService.auth.keycloak = keycloakAuth;
+          KeycloakService.auth.logoutUrl = keycloakAuth.authServerUrl
+            + '/realms/Pizza-Online-Realm/protocol/openid-connect/logout?redirect_uri='
+            + document.baseURI;
+          console.log(KeycloakService.auth);
           resolve();
         })
         .error(() => {
@@ -30,8 +56,26 @@ export class KeycloakService {
     });
   }
 
-  getToken(): string {
-    return this.keycloakAuth.token;
+  getToken(): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      if (KeycloakService.auth.keycloak.token) {
+        KeycloakService.auth.keycloak
+          .updateToken(60)
+          .done((refreshed) => {
+            console.log('refreshed ' + refreshed);
+            resolve(<string>KeycloakService.auth.keycloak.token);
+          })
+          .error(() => {
+            reject('Failed to refresh token');
+          });
+      } else {
+        reject('Not logged in');
+      }
+    });
+  }
+
+  getParsedToken() {
+    return KeycloakService.auth.keycloak.tokenParsed;
   }
 
 }
