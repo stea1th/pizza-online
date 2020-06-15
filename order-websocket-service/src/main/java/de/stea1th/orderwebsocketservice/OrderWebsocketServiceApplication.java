455df14ff3f986b289f7a1ddadc90a2a5e022760
vlasov <stea1th@mail.ru>
2020-06-15 12:21:14 +0200

    websocket with zuul

---
+++ order-websocket-service/src/main/java/de/stea1th/orderwebsocketservice/OrderWebsocketServiceApplication.java
@@ -0,0 +1,13 @@
+package de.stea1th.orderwebsocketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderWebsocketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderWebsocketServiceApplication.class, args);
+    }
+
+}
---
+++ order-websocket-service/src/test/java/de/stea1th/orderwebsocketservice/OrderWebsocketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stea1th.orderwebsocketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWebsocketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
