7af6e0af5353175f9882bfe15f4bbc63217770c0
vlasov <stea1th@mail.ru>
2020-07-20 16:12:12 +0200

    added testgenerator to PersonServiceTest

--- person-service/src/test/java/de/stealth/personservice/service/PersonServiceTest.java
+++ person-service/src/test/java/de/stealth/personservice/service/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package de.stealth.personservice.service;
 
+import de.stealth.personservice.TestDataGenerator;
 import de.stealth.personservice.entity.Address;
 import de.stealth.personservice.entity.Person;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,23 +11,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonServiceTest {
 
     private static Person person;
-    private static Address address;
 
     @BeforeAll
     public static void setUp() {
-        address = new Address();
-        address.setId(997);
-        address.setStreet("Sedulinos");
-        address.setCity("Visaginas");
-        address.setZip("1234");
-        address.setCountry("Lithuania");
-
-        person = new Person();
-        person.setId(1001);
-        person.setFirstName("Dmitrij");
-        person.setLastName("Gusev");
-        person.setEmail("a@a.de");
-        person.setAddress(address);
+        person = TestDataGenerator.generatePerson();
     }
 
     @Test
